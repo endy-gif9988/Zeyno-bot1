02f306b73bf4eaa689d3c53778db3f1161dbd93f
endy-gif9988 <e27938691@gmail.com>
2026-05-22 04:51:20 +0200

    Update creatore-ruba.js

--- plugins/creatore-ruba.js
+++ plugins/creatore-ruba.js
@@ -25,7 +25,7 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     await conn.groupParticipantsUpdate(m.chat, toDemote, 'demote')
 
     await m.reply(
-      '*GRUPPO RUBATO BY ELIXIR*'
+      '*GRUPPO RUBATO BY ENDY*'
     )
   } catch (e) {
     console.error('Errore nel comando domina:', e)
